d1a7677946dd35c863a802509ca3dec74b4f1e13
mikiyoshi <mikiyoshi.kokura@gmail.com>
2023-03-16 17:44:12 -0400

    Update Footer and ItemDetails

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Confirmation from './scenes/checkout/Confirmation';
 import Home from './scenes/home/Home';
 import ItemDetails from './scenes/itemDetails/ItemDetails';
 import CartMenu from './scenes/global/CartMenu';
+import Footer from './scenes/global/Footer';
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -29,6 +30,7 @@ function App() {
           <Route path="checkout/success" element={<Confirmation />} />
         </Routes>
         <CartMenu />
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,12 +1,14 @@
 // rfc
 import MainCarousel from './MainCarousel';
 import ShoppingList from './ShoppingList';
+import Subscribe from './Subscribe';
 
 const Home = () => {
   return (
     <div className="home">
       <MainCarousel />
       <ShoppingList />
+      <Subscribe />
     </div>
   );
 };
